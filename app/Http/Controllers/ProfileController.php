e01dadb9efc5dd1291cd7a703b0664e460aa948e
LSDUFFY <esatefekaralar@hotmail.com>
2024-10-21 15:19:47 +0300

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -38,7 +38,7 @@ class ProfileController extends Controller
     }
 
     /**
-     * Delete the user's account.
+     * Delete the user's account..
      */
     public function destroy(Request $request): RedirectResponse
     {
