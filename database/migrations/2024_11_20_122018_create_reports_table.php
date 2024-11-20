c63feac2ce2a03bf9430f406b7301c5e7b57947f
aysegyilmaz <aysegyilmaz71@gmail.com>
2024-11-20 15:57:19 +0300

    Database
    yenilik

---
+++ database/migrations/2024_11_20_122018_create_reports_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->integer('reporter_id');
+            $table->integer('reported_id');
+            $table->string('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reports');
+    }
+};
